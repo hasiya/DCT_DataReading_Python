e09e38b243485f84f1807021e4fe1791249f446c
Rajith Hasith <hasith002@yahoo.com>
2016-06-06 15:47:48 +0100

    Started on Json Parsing

--- JSON/JSON.py
+++ JSON/JSON.py
@@ -4,11 +4,17 @@ def readfile(file):
     try:
         with open(file,'r') as jsonfile:
             file = json.load(jsonfile)
-            print()
-
+            process(file)
 
     except FileNotFoundError as e:
         print(e)
+
+
+def process(file):
+    for key in file.keys():
+        print(key.items())
+        process(key)
+
 
 if __name__ == "__main__":
     readfile("web.json")
